eabccd3753198d528f8750b4ca6e1426d78086ac
hotequil <hotequil@protonmail.com>
2021-10-31 14:56:42 -0300

    done style files, footer and header.

---
+++ components/footer.php
@@ -0,0 +1,7 @@
+        </main>
+
+        <footer class="footer content">
+            <p class="footer__text">By João Paulo Hotequil</p>
+        </footer>
+    </body>
+</html>
---
+++ components/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <title>Beers</title>    
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="CRUD in PHP for beers">
+        <meta name="keywords" content="crud, php, beers">
+        <meta name="author" content="João Paulo Hotequil">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="copyright" content="João Paulo Hotequil">
+        <meta name="rating" content="general">
+        <link href="./images/favicon.ico" type="image/x-icon" rel="icon">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="./stylesheets/styles.css">
+    </head>
+    <body class="body">
+        <header class="header">
+            <div class="header__content content">
+                <a href="./index.php" target="_self" aria-label="Go to main page">
+                    <img class="header__image" src="./images/logo.png" alt="Beers logo">
+                </a>
+
+                <h1 class="header__title">Beers</h1>
+            </div>
+        </header>
+
+        <main class="content main">
